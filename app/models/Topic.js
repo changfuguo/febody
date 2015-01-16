c3f0ea606ec64fae875162664a3c84a6ead8e51a
changfuguo <changfuguo@qq.com>
2015-01-16 12:57:46 +0800

    add files

---
+++ app/models/Topic.js
@@ -0,0 +1,37 @@
+
+//topic for 
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var TopicSchema = new Schema({
+        
+        title        :   {type: String},
+        content      :   {type: String},
+        author_id    :   {type: ObjectId},
+        top          :   {type: Boolean, default: false},
+        good         :   {type: Boolean, default: false},
+        reply_count  :   {type: Number, default: 0},
+        visit_count  :   {type: Number, default: 0},
+        create_at    :   {type: Date, default: Date.now},
+        update_at    :   {type: Date, default: Date.now},
+        last_reply   :   {type: ObjectId, default: Date.now},
+        last_reply_at:   {type: Date, default: Date.now},
+        ishtml       :   {type: Boolean},
+        status       :   {type: Number,default: 1} , //1 normal ;0 : disabled
+    
+    });
+
+
+TopicSchema.index({create_at: -1});
+TopicSchema.index({top: -1 ,last_reply_at: -1});
+
+TopicSchema.index({last_reply_at: -1});
+TopicSchema.index({author_id: 1, create_at: -1});
+
+
+
+
+mongoose.model('Topic',TopicSchema);
