a6686cc3292d3d3c1f4ab72f27661f02ed2d64b6
changfuguo <changfuguo@qq.com>
2015-01-16 13:39:51 +0800

    update commit

--- config/config.js
+++ config/config.js
@@ -15,7 +15,6 @@ var config = {
             name : "前端body",
             keywords:"javascript,html,css",
             port : 13003 ,
-            host :"http://182.92.98.242",
             session_secret:'febody-session-secret', // session secret 
             auth_cookie_name:'febody',              // powerful user name
             //topic list count
