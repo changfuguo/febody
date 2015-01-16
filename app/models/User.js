93f642f83c6caa096f11de98dca682aea4adfe4d
changfuguo <changfuguo@qq.com>
2015-01-16 13:37:37 +0800

    add update

--- app/models/User.js
+++ app/models/User.js
@@ -5,3 +5,29 @@
 var mongoose = require('mongoose');
 var Schema   = mongoose.Schema ;
 
+
+var UserSchema = new Schema({
+        name :{ type : String},
+        loginname:{type : String},
+        pass :{type : String},
+        email:{type:String},
+        signature :{type : String},
+        profile_image_url :{type:String},
+
+        create_at: { type: Date, default: Date.now },
+        update_at: { type: Date, default: Date.now }, 
+   
+        status :{type:String} , // 1 :normal ;0:disabled
+        last_login_time :{type:Date , default : Date.now},
+        accessToken :{type:String},
+        
+        lastLogin :{type:Date,Default:Date.now}
+    
+    });
+
+
+UserSchema.index({loginname:1},{unique:true});
+UserSchema.index({email:1},{unique:true});
+UserSchema.index({accessToken:1});
+
+mongoose.model('User',UserSchema);
--- config/config.js
+++ config/config.js
@@ -8,26 +8,23 @@ var rootPath = path.join(path.normalize(__dirname + '/..') + '/')
 
 var config = {
     
-        'development' :{
             debug:true,
+
             db : "mongodb://febody:febody@localhost:17017/febody" ,  
             rootpath : rootPath ,
             name : "前端body",
             keywords:"javascript,html,css",
             port : 13003 ,
             host :"http://182.92.98.242",
-            session_secret:'febody-session-secret',
-            auth_cookie_name:'febody',
+            session_secret:'febody-session-secret', // session secret 
+            auth_cookie_name:'febody',              // powerful user name
+            //topic list count
+            list_topic_count : 20 ,
             admins:{user_login_name : true },
             upload:{
-                path:path.join(rootPath,'uploads/')
+                path:path.join(rootPath,'/public/uploads/'),
+                url : '/public/uploads/'
             }
-         },
-         'online':{
-                         
-             
-          }
-    
 }
 
 
