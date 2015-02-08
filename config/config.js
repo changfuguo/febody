e14ebac5d34953a909025b5c91f7b79f06c782d3
changfuguo <changfuguo@qq.com>
2015-02-08 23:31:32 +0800

    update config

--- config/config.js
+++ config/config.js
@@ -10,13 +10,15 @@ var config = {
     
             debug:true,
 
-            db : "mongodb://febody:febody@localhost:17017/febody" ,  
+            db : "mongodb://test:test@localhost:17017/test" ,  
             rootpath : rootPath ,
             name : "前端body",
             keywords:"javascript,html,css",
             port : 13003 ,
-            session_secret:'febody-session-secret', // session secret 
-            auth_cookie_name:'febody',              // powerful user name
+            host :"localhost",
+            session_secret:'test', // session secret 
+            session_timeout : 24 * 60 * 60,
+            auth_cookie_name:'testtesttest',              // powerful user name
             //topic list count
             list_topic_count : 20 ,
             admins:{user_login_name : true },
