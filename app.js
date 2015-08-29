dd9463088a46278ff183bd68544dfff938e5fac9
changfuguo <changfuguo@qq.com>
2015-08-29 16:47:50 +0800

    add gulp project

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var config = require('./config/config');
 var app = express();
 
+var log4js = require('log4js');
+log4js.configure('./config/log4js.json');
+
 require('./app/models');
 
 require('./config/express')(app);
 
 require('./config/routes')(app);
 
+var log  = log4js.getLogger('app');
 
 //listen port
-
 app.listen(config.port);
 module.exports = app;
